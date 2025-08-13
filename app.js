b3f1ebf68f365c18e39a291cb27b1427df36ed8d
GAURAVKAPRE <gauravkapre49@gmail.com>
2025-08-13 21:27:23 +0530

    add this

---
+++ app.js
@@ -0,0 +1,42 @@
+/**
+ * 
+ * 
+ * <div id="parent">
+ *      <div id="child1">
+ *     <h1>i'm h1 tag</h1>
+ *     <h2>i'm h1 tag</h2>
+      </div>
+<div id="child2">
+ *     <h1>i'm h1 tag</h1>
+ *     <h2>i'm h1 tag</h2>   
+     </div>
+   </div>
+ */
+     // reactElement(object)=>html(browser understands)
+
+
+
+      const parent = React.createElement(
+        "div",{id:"parent"},[
+        React.createElement(
+            "div",{id:"child"},
+           [React.createElement("h1",{},"i'm an h1 tag"),
+            React.createElement("h2",{},"i'm an h1 tag"),
+           ]),
+            React.createElement(
+            "div",{id:"child"},
+           [React.createElement("h1",{},"i'm an h1 tag"),
+            React.createElement("h2",{},"i'm an h1 tag"),
+           ]) ]
+    );
+
+
+const heading =  React.createElement("h1" , {id:"heading"} , "hello world from react !") ;
+   const root = ReactDOM.createRoot(document.getElementById("root"));
+
+  //  console.log(heading);// object 
+  //  root.render(heading);
+
+   console.log(parent);// object 
+   root.render(parent);
+
