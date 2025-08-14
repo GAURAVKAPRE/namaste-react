aa454c40092c96f6a20a95607eeea55c448f64c5
GAURAVKAPRE <gauravkapre49@gmail.com>
2025-08-14 23:41:05 +0530

    episode 3

--- app.js
+++ app.js
@@ -1,27 +1,51 @@
-   import React  from "react";
-   import ReactDOM from "react-dom/client";
-   
-   const parent = React.createElement(
-        "div",{id:"parent"},[
-        React.createElement(
-            "div",{id:"child"},
-           [React.createElement("h1",{},"i'm an h1 tag"),
-            React.createElement("h2",{},"JAI HIND , FIGHT MARNA HAI BOSS"),
-           ]),
-            React.createElement(
-            "div",{id:"child"},
-           [React.createElement("h1",{},"i'm an h1 tag"),
-            React.createElement("h2",{},"i'm an h1 tag"),
-           ]) ]
+ import React  from "react";
+import ReactDOM from "react-dom/client";
+
+// React.createElement => reactElement-JS object => htmlelement(render);
+
+//JSX - html-like or xml-like syntax 
+
+// jsx(transpiled before it reaches the js ) - parcel - Bebel
+
+
+//JSX => Babel transpiles it to React.createElement =>ReactElement-Js object =>HTMLElement(render);
+
+const elem = <span>react element</span>;
+
+const jsxheading =(<h1 id="heading">
+    namaste react using jsx ;
+     {elem}
+    </h1>
+    );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxheading);
+
+// react component 
+// class based components - old 
+// functional components - new 
+
+//React functional components
+
+//component composition
+const Title =() =>(
+    <h1 className="head" >namaste react using jsx</h1>
+ );
+
+ const number = 1000;
+ const HeadingComponent2 =() =>(
+    <div id="container">
+         {jsxheading}
+         {Title()};
+         <Title/>
+         <Title></Title>
+
+    <h1 className="heading">namaste react functional component</h1>
+    </div>
     );
 
 
-const heading =  React.createElement("h1" , {id:"heading"} , "hello world from react !") ;
-   const root = ReactDOM.createRoot(document.getElementById("root"));
 
-  //  console.log(heading);// object 
-  //  root.render(heading);
-
-   console.log(parent);// object 
-   root.render(parent);
+root.render(<HeadingComponent2/>);
 
