20c176e6250a2a1e45059d80b9703bf6a439e237
GAURAVKAPRE <gauravkapre49@gmail.com>
2025-08-17 22:11:26 +0530

    episode 5 done

--- src/component/Body.js
+++ src/component/Body.js
@@ -15,66 +15,14 @@ const[listOfRestaurent, setListofRestaurent]= useState(resList);
 //    let listOfRestaurent = [];
    
     //normal js variable
-    let listOfRestaurent2 = [ {
-                "card": {
-                    "card": {
-                        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
-                        "info": {
-                            "id": "705473",
-                            "name": "Delhi Kitchen",
-                            "cloudinaryImageId": "ee0c86496cd11e53724dda5ca6067fad",
-                            "locality": "Opp Shanti Nagar",
-                            "areaName": "Bibwewadi",
-                            "costForTwo": "₹250 for two",
-                            "cuisines": [
-                                "North Indian",
-                                "Chinese",
-                                "South Indian",
-                                "Snacks",
-                                "Biryani",
-                                "Thalis",
-                                "Beverages"
-                            ],
-                            "avgRating": 4.1,
-                    
-                            }
-                        }
-                    }
-      
-            }, {
-                "card": {
-                    "card": {
-                        "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
-                        "info": {
-                            "id": "7052322",
-                            "name": "bhai ka dhaba",
-                            "cloudinaryImageId": "ee0c86496cd11e53724dda5ca6067fad",
-                            "locality": "Opp Shanti Nagar",
-                            "areaName": "Bibwewadi",
-                            "costForTwo": "₹250 for two",
-                            "cuisines": [
-                                "North Indian",
-                                "Chinese",
-                                "South Indian",
-                                "Snacks",
-                                "Biryani",
-                                "Thalis",
-                                "Beverages"
-                            ],
-                            "avgRating": 3.6,
-                    
-                            }
-                        }
-                    }
-      
-            }];
+   
     return (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" onClick={()=>{
                     // filter logic here 
                     const filterList  = listOfRestaurent.filter(
-                        (res) =>res.card.card.info.avgRating > 4 
+                        (res) =>res.card.card.info.avgRating > 4.5
                     );
                  setListofRestaurent(filterList);
 
